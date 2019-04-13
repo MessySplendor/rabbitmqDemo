00f2cc5ffa48c65af10914c90f152264d4629368
lhd <1031980490@qq.com>
2019-04-13 11:26:27 +0800

    i md simple   CONSUSER 2019-04-13 03:26:27

--- simpleConsum.php
+++ simpleConsum.php
@@ -1,9 +1,9 @@
     <?php
 
 
-    require_once __DIR__ . '/vendor/autoload.php';  
+    require_once '../vendor/autoload.php';  
     use PhpAmqpLib\Connection\AMQPStreamConnection;  
-    $connection = new AMQPStreamConnection('172.17.0.5', 5672, 'admin', 'admin123456','msg');  
+    $connection = new AMQPStreamConnection('172.17.0.5', 5672, 'liuhaidong', '123456','kangbazi');  
     $channel = $connection->channel();  
       
     $channel->queue_declare('hello', false, false, false, false);  

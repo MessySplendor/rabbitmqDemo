163e22795854b36a4021184d5f04952482a1dbc1
lhd <1031980490@qq.com>
2019-05-13 17:20:03 +0800

    test my gitee  time  2019-05-13 09:20:03

--- workerReciveOne.php
+++ workerReciveOne.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/vendor/autoload.php';
+require_once '../vendor/autoload.php';   
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 
 $connection = new AMQPStreamConnection('172.17.0.5', 5672, 'dabaobei', '123456','dabaobei');
